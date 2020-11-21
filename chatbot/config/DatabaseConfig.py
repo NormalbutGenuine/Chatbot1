fa2584ba8d41cf514e12e5590cce31d4700661b3
NormalbutGenuine <69850657+NormalbutGenuine@users.noreply.github.com>
2020-11-21 20:18:49 +0900

    Update DatabaseConfig.py

--- chatbot/config/DatabaseConfig.py
+++ chatbot/config/DatabaseConfig.py
@@ -1,7 +1,7 @@
 DB_HOST = "database-chatbot.cewgljvqav4v.ap-northeast-2.rds.amazonaws.com"
-DB_USER = "database-chatbot"
+DB_USER = "admin"
 DB_PORT = 3306
 DB_NAME = "chatbotdb"
-DB_PASSWORD = ""
+DB_PASSWORD = "chatbotdb"
 def DatabaseConfig():
     global DB_HOST, DB_USER, DB_PORT, DB_NAME
