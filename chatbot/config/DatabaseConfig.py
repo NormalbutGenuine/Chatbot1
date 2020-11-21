727ac4d7a40e0bc70a3d7c1e6ce22f5c14038de6
NormalbutGenuine <69850657+NormalbutGenuine@users.noreply.github.com>
2020-11-21 18:06:17 +0900

    Update DatabaseConfig.py
    RDS정보

--- chatbot/config/DatabaseConfig.py
+++ chatbot/config/DatabaseConfig.py
@@ -1,7 +1,7 @@
-DB_HOST = "127.0.0.1"
-DB_USER = "root"
-DB_PASSWORD = "tiger"
-DB_NAME = "chatbot"
+DB_HOST = "database-chatbot.cewgljvqav4v.ap-northeast-2.rds.amazonaws.com"
+DB_USER = "database-chatbot"
+DB_PORT = "3306"
+DB_NAME = "chatbotdb"
 
 def DatabaseConfig():
-    global DB_HOST, DB_USER, DB_PASSWORD, DB_NAME
+    global DB_HOST, DB_USER, DB_PORT, DB_NAME
